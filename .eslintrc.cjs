ad5cce8e13b8487ba27867d121556a4e0692d4a3
huangxiuye <781662424@qq.com>
2023-02-14 10:19:18 +0800

    feat :布局新增

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,20 +4,13 @@ module.exports = {
     es2021: true,
     node: true
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    'standard-with-typescript',
-    'plugin:prettier/recommended'
-  ],
-  overrides: [
-  ],
+  extends: ['plugin:vue/vue3-essential', 'standard-with-typescript', 'plugin:prettier/recommended'],
+  overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
-  plugins: [
-    'vue'
-  ],
+  plugins: ['vue'],
   rules: {
     'vue/multi-word-component-names': 'off'
   }
