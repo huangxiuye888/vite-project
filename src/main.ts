c6a9615065bbd73b8dbcdf32e97a86b93432d0a6
huangxiuye <781662424@qq.com>
2023-02-14 09:53:22 +0800

    feat :代码风格添加

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import router from './router/index'
+import store from './store/index'
+import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
+const app = createApp(App)
+app.use(store).use(router).use(ElementPlus).mount('#app')
